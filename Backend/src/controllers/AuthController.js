b6c901be7ea1e890d231720ac736b93c9dbc7abe
JonathanAristides <Jonathan.nuel98@gmail.com>
2023-05-17 16:37:26 +0200

    kind of working i guess, i dont know

--- Backend/src/controllers/AuthController.js
+++ Backend/src/controllers/AuthController.js
@@ -13,23 +13,29 @@ class AuthController {
       const token = helper.createToken(user._id);
       console.log(token)
       res.set('Set-Cookie', `jwt=${token}`);
-      res.redirect('/home');
+      res.redirect('http://192.168.32.1:8080/home.html');
     } catch (err) {
       const errorAnswer = helper.createNewMonError(err, 500, "Error creating user");
-      res.status(500).json(errorAnswer);
+
+      res.redirect(`http://192.168.32.1:8080/index.html?error=FEHLER`);
     }
   }
 
   async login(req, res) {
     const {email, password} = req.body;
 
+
     try {
       const user = await User.login(email, password);
-      res.status(200).json({user: user._id});
-
+      const token = helper.createToken(user._id);
+      res.set('Set-Cookie', `jwt=${token}`);
+      res.redirect('/home');
     } catch (err) {
       const errorAnswer = helper.createNewMonError(err, 500, "Error logging in");
-      res.status(500).json(errorAnswer);
+
+
+
+      res.redirect(`http://192.168.32.1:8080/login.html?error=${err.message}&email=${email}`);
     }
 
   }

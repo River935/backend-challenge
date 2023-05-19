143912bd1c895888d91889d9efbe1f73a08ad1e3
JonathanAristides <Jonathan.nuel98@gmail.com>
2023-05-19 12:22:39 +0200

    Login working

--- Backend/src/controllers/AuthController.js
+++ Backend/src/controllers/AuthController.js
@@ -1,45 +1,50 @@
 const helper = require("../helpers/Helper");
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
-
+const domain = "http://localhost:8080";
 
 class AuthController {
   async signUp(req, res) {
-
     const {name, password, email} = req.body;
 
     try {
       const user = await User.create({name, password, email});
+      console.log("User created");
+      console.log(user);
       const token = helper.createToken(user._id);
-      res.set('Set-Cookie', `jwt=${token}; httpOnly; domain=localhost`);
-      res.redirect('http://localhost:8080/home.html');
+
+      const cookieJwt = `jwt=${token}; httpOnly; domain=localhost`;
+      const cookieUser = `user=${user._id}; httpOnly; domain=localhost`;
+      const cookieRole = `role=${user.role}; httpOnly; domain=localhost`;
+      res.set("Set-Cookie", [cookieJwt, cookieUser, cookieRole]);
+
+      res.redirect(`${domain}/home.html`);
     } catch (err) {
-      console.log(err.message);
-      res.redirect(`http://localhost:8080/index.html?error=${err.message}&name=${name}&email=${email}`);
+      const errorAnswer = helper.createNewMonError(err, 400, "Error signing up!");
+      console.log(errorAnswer);
+      res.redirect(`${domain}/index.html?error=${errorAnswer.description}, ${errorAnswer.message}&name=${name}&email=${email}`);
     }
   }
 
-    async login(req, res)
-    {
-      const {email, password} = req.body;
+  async login(req, res) {
+    const {email, password} = req.body;
 
-      try {
-        const user = await User.login(email, password);
-        const token = helper.createToken(user._id);
-        res.set('Set-Cookie', `jwt=${token}`);
-        res.redirect('/home');
-      } catch (err) {
-        const errorAnswer = helper.createNewMonError(err, 500, "Error logging in");
+    try {
+      const user = await User.login(email, password);
+      const token = helper.createToken(user._id);
 
+      const cookieJwt = `jwt=${token}; httpOnly; domain=localhost`;
+      const cookieUser = `user=${user._id}; httpOnly; domain=localhost`;
+      const cookieRole = `role=${user.role}; httpOnly; domain=localhost`;
+      res.set("Set-Cookie", [cookieJwt, cookieUser, cookieRole]);
 
-        res.redirect(`http://192.168.32.1:8080/login.html?error=${err.message}&email=${email}`);
-      }
-
+      res.redirect(`${domain}/home.html`);
+    } catch (err) {
+      const errorAnswer = helper.createNewMonError(err, 400, "Error logging in");
+      console.log(errorAnswer);
+      res.redirect(`${domain}/login.html?error=${errorAnswer.description}, ${errorAnswer.message}&email=${email}`);
     }
   }
+}
 
-  module
-
-.
-
-  exports = AuthController;
+module.exports = AuthController;
--- Backend/src/models/userModel.js
+++ Backend/src/models/userModel.js
@@ -1,44 +1,45 @@
-const {Schema, model} = require('mongoose');
-const {isEmail} = require('validator');
-const bcrypt = require('bcrypt');
+const {Schema, model} = require("mongoose");
+const {isEmail} = require("validator");
+const bcrypt = require("bcrypt");
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Enter a Name"],
-    lowercase: true,
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Enter a Name"],
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Enter an Email"],
+      unique: true,
+      lowercase: true,
+      validate: [isEmail, "Enter a valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Enter a password"],
+      minlength: [4, "Password must be at least 4 characters long"],
+    },
+    role: {
+      type: String,
+      default: "user",
+      enum: ["admin", "user"],
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Enter an Email"],
-    unique: true,
-    lowercase: true,
-    validate: [isEmail, "Enter a valid email"],
-  },
-  password: {
-    type: String,
-    required: [true, "Enter a password"],
-    minlength: [4, "Password must be at least 4 characters long"]
-  },
-  role: {
-    type: String,
-    default: 'user',
-    enum: ['admin', 'user'],
-  }
-}, {collection: 'Users'});
+  {collection: "Users"}
+);
 
 userSchema.post("save", function (doc, next) {
-    console.log("New user was created and saved", doc);
-    next();
-  }
-);
+  console.log("New user was created and saved", doc);
+  next();
+});
 
 userSchema.pre("save", async function (next) {
-    const salt = await bcrypt.genSalt();
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  }
-);
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
 userSchema.statics.login = async function (email, password) {
   const user = await this.findOne({email});
@@ -50,8 +51,8 @@ userSchema.statics.login = async function (email, password) {
     throw Error("Incorrect password");
   }
   throw Error("Incorrect email");
-}
+};
 
-const Users = model('Users', userSchema);
+const Users = model("Users", userSchema);
 
 module.exports = Users;
